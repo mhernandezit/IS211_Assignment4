080a28cfb117b8663bc0deaa7298d4061ac0407b
mhernandezit <mhernandezit@gmail.com>
2019-02-26 08:29:06 -0500

    Further linting, commenting code, some minor refactoring

--- search_compare.py
+++ search_compare.py
@@ -4,7 +4,16 @@ import time
 import random
 
 def sequential_search(a_list, item):
-    """ Sequential Search """
+    """ Sequential Search
+    Args:
+        a_list (list) A list of integers
+        item (int) The number we will be searching for
+
+    Returns:
+        found (boolean) True if the item is in the list
+        algotime (float) Time the algorithm took to run
+
+    """
     pos = 0
     found = False
     start = time.time()
@@ -14,10 +23,19 @@ def sequential_search(a_list, item):
         else:
             pos = pos+1
     end = time.time()
-    return found, (end - start)
+    algotime = end - start
+    return found, algotime
 
 def ordered_sequential_search(a_list, item):
-    """ Ordered Sequential Search """
+    """ Ordered Sequential Search
+    Args:
+        a_list (list) A list of integers
+        item (int) The number we will be searching for
+
+    Returns:
+        found (boolean) True if the item is in the list
+        algotime (float) Time the algorithm took to run
+    """
     pos = 0
     found = False
     stop = False
@@ -34,7 +52,16 @@ def ordered_sequential_search(a_list, item):
     return found, (end - start)
 
 def binary_search(a_list, item):
-    """ Binary Search """
+    """ Binary Search
+    Args:
+        a_list (list) A list of integers
+        item (int) The number we will be searching for
+
+    Returns:
+        found (boolean) True if the item is in the list
+        algotime (float) Time the algorithm took to run
+
+    """
     start = time.time()
     first = 0
     last = len(a_list) - 1
@@ -52,8 +79,15 @@ def binary_search(a_list, item):
     return found, (end - start)
 
 def recursive_binary_search(a_list, item):
-    """ Recursive Binary Search """
-    if len(a_list) == 0:
+    """ Recursive Binary Search
+    Args:
+        a_list (list) A list of integers
+        item (int) The number we will be searching for
+
+    Returns:
+        found (boolean) True if the item is in the list
+    """
+    if not a_list:
         return False
     else:
         midpoint = len(a_list) // 2
@@ -66,35 +100,50 @@ def recursive_binary_search(a_list, item):
             return recursive_binary_search(a_list[midpoint + 1:], item)
 
 def buildList(listSize):
-    """ A random list builder """
+    """ A random list builder
+    Args:
+        listSize (int) The size of the list we want to build
+
+    Returns:
+        randomList (list) A randomly generated list
+
+    """
     randomList = random.sample(xrange(1, listSize+100), listSize)
     return randomList
 
 def runTests(testSize, listSize):
-    """ Builds out our test suites """
+    """ Builds out our test suites, prints results of search algorithms
+    Args:
+        testSize (int) How many tests we want to run
+        listSize (int) How big our random lists will be
+
+    Returns:
+        Null
+
+    """
     testResults = {'Sequential Search': {'time': 0},
                    'Ordered Sequential Search': {'time': 0},
                    'Binary Search': {'time': 0},
                    'Recursive Binary Search': {'time': 0}}
-    for x in xrange(testSize):
-        testlist = buildList(listSize)
-        for k, v in testResults.items():
-            if k == 'Sequential Search':
-                testResults[k]['time'] += sequential_search(testlist, -1)[1]
-            if k == 'Ordered Sequential Search':
+    for _ in xrange(testSize):
+        testlist = buildList(listSize) # We are only going to build the random list once
+        for key, value in testResults.items():
+            if key == 'Sequential Search': # Sequential search works on a unsorted list
+                testResults[key]['time'] += sequential_search(testlist, -1)[1]
+            if key == 'Ordered Sequential Search':
                 list.sort(testlist)
-                testResults[k]['time'] += ordered_sequential_search(testlist, -1)[1]
-            if k == 'Binary Search':
+                testResults[key]['time'] += ordered_sequential_search(testlist, -1)[1]
+            if key == 'Binary Search':
                 list.sort(testlist)
-                testResults[k]['time'] += binary_search(testlist, -1)[1]
-            if k == 'Recursive Binary Search':
+                testResults[key]['time'] += binary_search(testlist, -1)[1]
+            if key == 'Recursive Binary Search':
                 list.sort(testlist)
-                rb = time.time()
+                rbTime = time.time()
                 recursive_binary_search(testlist, -1)
-                testResults[k]['time'] += time.time() - rb
-    for k, v in testResults.items():
-        average = v['time']/listSize
-        print '{} took {} seconds to run, on average.'.format(k, "%10.7f" % average)
+                testResults[key]['time'] += time.time() - rbTime
+    for key, value in testResults.items():
+        average = value['time']/listSize
+        print '{} took {} seconds to run, on average.'.format(key, "%10.7f" % average)
 
 def main():
     """ Main excecution method """
@@ -103,7 +152,7 @@ def main():
     print 'Test 2, 1000 item lists'
     runTests(100, 1000)
     print 'Test 3, 10000 item lists'
-    runTests(100, 10000)
+    #runTests(100, 10000)
 
 if __name__ == '__main__':
     main()
